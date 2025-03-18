e67ab7e9a8938b7a83833950a934a7ed5be112cb
carlospinheiro <cadutapinheiro@gmail.com>
2025-03-18 15:36:37 -0300

    route create user

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/create-user', [UserController::class, 'create'])->name('user.create');
