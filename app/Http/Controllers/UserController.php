8f45a365685e4b9aa08861d01c5210bdf7671afb
carlospinheiro <cadutapinheiro@gmail.com>
2025-03-27 10:01:21 -0300

    actions buttons styles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function index() 
     {
-        $users = User::orderByDesc('id')->paginate(10);
+        $users = User::orderByDesc('id')->paginate(3);
 
         return view('users.index', ['users' => $users ]);        
         
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -72,8 +72,10 @@
                                     admin</td>
                                 <td class="px-4 py-3"> {{ $user->created_at }} </td>
                                 <td class="px-4 py-3"> {{ $user->updated_at }}</td>
-                                <td class="px-4 py-3 flex items-center justify-end">
-                                    <button class="px-3 py-1 bg-red-500 text-white rounded">X</button>
+                                <td class="px-4 py-3 flex items-center gap-2 justify-end">
+                                    <button class="btn-primary">Visualizar</button>
+                                    <button class="btn-warning">Editar</button>
+                                    <button class="btn-danger">Apagar</button>
                                 </td>
                             </tr>
                             @empty
@@ -92,8 +94,10 @@
   
                   <div class="py-4 px-3">
                       <div class="flex ">
-                          <div class="flex space-x-4 items-center mb-3">
-                              <label class="w-32 text-sm font-medium text-gray-900">Per Page</label>
+                          <div class="w-full">
+                            {{ $users->links() }}
+
+                              {{-- <label class="w-32 text-sm font-medium text-gray-300">Per Page</label>
                               <select
                                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
                                   <option value="5">5</option>
@@ -101,7 +105,7 @@
                                   <option value="20">20</option>
                                   <option value="50">50</option>
                                   <option value="100">100</option>
-                              </select>
+                              </select> --}}
                           </div>
                       </div>
                   </div>
